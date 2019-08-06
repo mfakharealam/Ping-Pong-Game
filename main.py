e5841ebc270349c765fdebec8a8e997dd441ef91
MFA571 <fakhar571@gmail.com>
2019-08-06 23:52:56 +0500

    added both paddles

--- main.py
+++ main.py
@@ -11,10 +11,18 @@ paddle_one = turtle.Turtle()
 paddle_one.speed(0)     # speed of animation, '0' for MAX
 paddle_one.color("white")
 paddle_one.shape("square")
+paddle_one.shapesize(stretch_wid=5, stretch_len=1)
 paddle_one.penup()
 paddle_one.goto(-350, 0)    # (0, 0) is in middle
-#   2nd Paddle
 
+#   2nd Paddle
+paddle_two = turtle.Turtle()
+paddle_two.speed(0)     # speed of animation, '0' for MAX
+paddle_two.color("white")
+paddle_two.shape("square")
+paddle_two.shapesize(stretch_wid=5, stretch_len=1)
+paddle_two.penup()
+paddle_two.goto(350, 0)    # (0, 0) is in middle
 #   Ball
 
 
