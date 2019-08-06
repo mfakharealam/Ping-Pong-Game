4f691341eab3aeefedc40ff6393c55707fea2115
MFA571 <fakhar571@gmail.com>
2019-08-06 23:47:30 +0500

    added main loop and window

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import turtle
 
+window = turtle.Screen()
+window.title("Ping Pong")
+window.bgcolor("black")
+window.setup(width=800, height=600)
+window.tracer(0)    # stops the window from updating
+
+# main loop for the game to run
+while True:
+    window.update()
+
