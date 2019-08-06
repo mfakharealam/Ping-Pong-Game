34e3429851edca01bae660a7ca80fc7a404c7a6c
MFA571 <fakhar571@gmail.com>
2019-08-06 23:50:40 +0500

    added a paddle

--- main.py
+++ main.py
@@ -6,6 +6,19 @@ window.bgcolor("black")
 window.setup(width=800, height=600)
 window.tracer(0)    # stops the window from updating
 
+#   1st Paddle
+paddle_one = turtle.Turtle()
+paddle_one.speed(0)     # speed of animation, '0' for MAX
+paddle_one.color("white")
+paddle_one.shape("square")
+paddle_one.penup()
+paddle_one.goto(-350, 0)    # (0, 0) is in middle
+#   2nd Paddle
+
+#   Ball
+
+
+
 # main loop for the game to run
 while True:
     window.update()
