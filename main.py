bfe209f23f77fee0abb813df7336f4cf5d2b2e14
MFA571 <fakhar571@gmail.com>
2019-08-07 00:23:57 +0500

    added paddles movement

--- main.py
+++ main.py
@@ -33,6 +33,40 @@ ball.penup()
 ball.goto(0, 0)    # (0, 0) is in middle
 
 
+# movement of paddle
+def paddle_one_up():
+    y = paddle_one.ycor()   # coordinates
+    y += 30
+    paddle_one.sety(y)
+
+
+def paddle_one_down():
+    y = paddle_one.ycor()   # coordinates
+    y -= 30
+    paddle_one.sety(y)
+
+
+def paddle_two_up():
+    y = paddle_two.ycor()   # coordinates
+    y += 30
+    paddle_two.sety(y)
+
+
+def paddle_two_down():
+    y = paddle_two.ycor()   # coordinates
+    y -= 30
+    paddle_two.sety(y)
+
+
+# Keyboard Events
+window.listen()
+# Left one
+window.onkeypress(paddle_one_up, 'w')
+window.onkeypress(paddle_one_down, 's')
+# right one
+window.onkeypress(paddle_two_up, 'Up')
+window.onkeypress(paddle_two_down, 'Down')
+
 # main loop for the game to run
 while True:
     window.update()
